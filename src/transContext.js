6d686df5c19e82f53dfdbdf3ce9722419a20ee00
Abdul Mobeen <mobeendev@gmail.com>
2020-06-30 16:40:34 +0300

    Added Remove Method

--- src/transContext.js
+++ src/transContext.js
@@ -1,35 +1,46 @@
 import React, { createContext, useReducer } from "react";
-import TransactionReducer from './transReducer';
+import TransactionReducer from "./transReducer";
 
 const initialTransactions = [
-    { amount: 500, desc: "Cash" },
-    { amount: -50, desc: "Cold Drink" },
-    { amount: 100, desc: "Deposit" },
-    { amount: -200, desc: "Utility Bill" },
-
-]
+  { id: 0, amount: 500, desc: "Cash" },
+  { id: 1, amount: -50, desc: "Cold Drink" },
+  { id: 2, amount: 100, desc: "Deposit" },
+  { id: 3, amount: -200, desc: "Utility Bill" },
+];
 
 export const TransactionContext = createContext(initialTransactions);
 
-export const TransactionProvider = ({children})=> {
-    let [state, dispatch] = useReducer(TransactionReducer, initialTransactions);
+export const TransactionProvider = ({ children }) => {
+  let [state, dispatch] = useReducer(TransactionReducer, initialTransactions);
 
-    function addTransaction(transObj){
-        dispatch({
-            type: "ADD_TRANSACTION",
-            payload: { 
-                amount: transObj.amount, 
-                desc: transObj.desc 
-            },
-        })
-    }
+  function addTransaction(transObj) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: {
+        id: transObj.id,
+        amount: transObj.amount,
+        desc: transObj.desc,
+      },
+    });
+  }
+  function removeTransaction(itemId) {
+    dispatch({
+      type: "REMOVE_TRANSACTION",
+      payload: {
+        deleteId: itemId,
+      },
+    });
+  }
 
-    return(
-        <TransactionContext.Provider value={{
-            transactions: state,
-            addTransaction
-        }}>
-            {children}
-        </TransactionContext.Provider>
-    )
-}
+  return (
+    <TransactionContext.Provider
+      value={{
+        transactions: state,
+        addTransaction,
+        removeTransaction,
+      }}
+    >
+      {children}
+    </TransactionContext.Provider>
+  );
+};
--- src/transReducer.js
+++ src/transReducer.js
@@ -1,11 +1,18 @@
-const TransactionReducer =  ((state, action)=>{
-    switch(action.type){
-        case "ADD_TRANSACTION": {
-            return [action.payload , ...state]
-        }
-        default:
-            return state;
+const TransactionReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TRANSACTION": {
+      return [action.payload, ...state];
     }
-})
+    case "REMOVE_TRANSACTION": {
+      let newState = state.filter(
+        (item) => item.id !== action.payload.deleteId
+      );
+
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
 
 export default TransactionReducer;
